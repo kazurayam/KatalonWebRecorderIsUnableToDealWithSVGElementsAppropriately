17b1b55d036e07632927eceba0aafe6caa0d2ac8
kazurayam <kazuaki.urayama@gmail.com>
2020-07-16 21:57:50 +0900

    change TC2: remove WebUI.click(), add WebUI.verifyElementPresent()

--- Scripts/TC2/Script1580442771792.groovy
+++ Scripts/TC2/Script1580442771792.groovy
@@ -7,6 +7,8 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://www.highcharts.com/demo/line-basic')
 
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Basic line  Highcharts/tspan_Installation - manually modified'), 10)
+
 WebUI.click(findTestObject('Object Repository/Page_Basic line  Highcharts/tspan_Installation - manually modified'))
 
 WebUI.delay(5)
